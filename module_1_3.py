615026b764c28f2350485119482aaf03ecc257ea
teestar5 <147434843+teestar5@users.noreply.github.com>
2024-10-12 23:16:24 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = "Name: Anatlii"
+print(name, type(name))
+age = "Age: 64"
+print(age, type(age))
+age = "New Age: 68"
+print(age, type(age))
+is_student ="is Student: True"
+print(is_student)
